b58e722fea5cf85c3f74327a07b12750ef1f5f28
ihcomega56 <ihcomega56@gmail.com>
2016-01-02 23:05:44 +0900

    Realm追加してみた

---
+++ MototoriCounter/RealmModel.swift
@@ -0,0 +1,19 @@
+//
+//  RealmModel.swift
+//  MototoriCounter
+//
+//  Created by Ayana Yokota on 2016/01/02.
+//  Copyright © 2016年 ihcomega. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmModel: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
